04e26f3a90317d36b156c2c26ae49613e9287adc
Yuka Matsuo <matsuo.yk@gmail.com>
2018-12-16 16:36:54 +0900

    Initial Commit

---
+++ Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by yuka on 2018/12/16.
+//  Copyright © 2018 Yuka Matsuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
